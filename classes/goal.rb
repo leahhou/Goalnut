4ba56d8172902f622ee16d001c62c545b47d8a34
Bryce harlum <bryce.harlum@gmail.com>
2019-03-05 15:51:44 +1100

    created menu for main menu + new method in goal to gather reports + started calling goal_prompts methods to initialize variables

--- classes/goal.rb
+++ classes/goal.rb
@@ -1,13 +1,22 @@
-require_relative 'goal_prompts'
+#require_relative 'goal_prompts'
 
 class Goal
     attr_reader :goal, :action, :time_frame, :report_frequency
-
-    def initialize(goal, action, time_frame, report_frequency)
-        @goal = goal
-        @action = action
-        @time_frame = time_frame
-        @report_frequency = report_frequency
+    gp = GoalPrompts.new
+    def initialize()
+        @goal = gp.choose_goal
+        @action = gp.choose_action
+        @time_frame = gp.set_times
+        @report_frequency = report_frequency#TODO change to method name from goal prompts
+        @reports = []
     end
 
+    #What did you achieve in the this reportable period?
+    ##Loop until all reports submitted
+    def gather_reports
+        i = @report_frequency
+        while i > 0
+        @reports << gets.chomp
+        end
+    end
 end
--- classes/menu.rb
+++ classes/menu.rb
@@ -1,18 +1,19 @@
 require_relative 'goal'
 
 class Menu
+
     def main_menu
         options = ["New Goal", "Quit"]
         quit = false
         while !quit
-            puts "Welcome to the banking app. What would you like to do?"
+            puts "Welcome to the Goals app. What would you like to do?"
             options.each do |item|
                 puts " #{item}"
             end
             input = gets.chomp.downcase
             case input
             when "new goal"
-                #when new goal
+                goal_ref = Goal.new
             when "quit"
                 quit = true
             end
