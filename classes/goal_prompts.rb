d5c643a388d8e072fd74f6dea745e552f5fbe2af
Leah Hou <hsx_124@hotmail.com>
2019-03-05 15:44:53 +1100

    define methods in goal_prompts.rb by leah

--- classes/goal_prompts.rb
+++ classes/goal_prompts.rb
@@ -1,5 +1,5 @@
 #take codes from goal.rb
-require_relative 'classes/goal.rb'
+require_relative 'goal.rb'
 
 
 ## Tests the given string to see if it equals 'yes'
@@ -17,33 +17,70 @@ def is_num(var)
 end
 
 goal = ""
-time_frame = ""
 action = ""
-report_frequency = ""
+time_frame = ""
 times_of_week = ""
 duration_of_action = ""
+report_frequency = ""
 
-#prompt users to enter their goal
-puts "Hi, would you like to create a new goal? [yes/no]" 
-input = gets.chomp 
-#create a control flow of yes and no after taking in users' answer input 
-#to see if they would like to continue the program
-if !is_yes?(input)
-    puts "As you do not want to set a goal; Exiting program"
-    exit
+
+#create a method called new_goal
+def new_goal
+    #prompt users to enter their goal
+    puts "Hi, would you like to create a new goal? [yes/no]" 
+    input = gets.chomp 
+    #create a control flow of yes and no after taking in users' answer input 
+    #to see if they would like to continue the program
+    if !is_yes?(input)
+        puts "As you do not want to set a goal; Exiting program"
+        exit
+    end
 end
 
-#prompt goal setting
-puts "What goal would you like to set today?"
-goal = gets.chomp
 
-#prompt action setting
-puts "what action would you like to do to work towards your goal?"
-action = gets.chomp 
+#create a method called choose_goal
+def choose_goal
+    #prompt goal setting
+    puts "What goal would you like to set today?" 
+    goal = gets.chomp
+end 
+#To do: give options to goals as exercise more, read more, eat healthier
 
-#prompt time_frame setting
-puts "what is the time frame[IN DAYS] of your action?"
-time_frame = gets.chomp
+
+
+#create a method called choose_action
+def choose_action
+    #prompt action setting
+    puts "what action would you like to do to work towards your goal?"
+    action = gets.chomp 
+end 
+##To do:give options to actions as swimming, jogging,  gym
+
+
+
+#create a method called set_times
+def set_times
+    puts "how many times a week would you like to go #{action}"
+    times_of_week = gets.chomp
+end 
+
+
+
+#create a method called set_duration
+def set_durations
+    puts "how long would you like to carry on your #{action} each time?"
+    duration_of_action = gets.chomp
+end
+##To do: add condition to enter in minutes
+
+
+
+#create a method called  set_timeframe
+def set_timeframe
+    #prompt time_frame setting
+    puts "what is the time frame[IN DAYS] of your action?"
+    time_frame = gets.chomp
+end
 
 # Checking to see if the entered input is > 7 days
 ## If input > 7 then it will print a message 
@@ -53,12 +90,11 @@ if time_frame.to_i >  7
     puts "ERROR: Maximum timeframe is a week. Maximum has been set to seven days."
 end
 
+#create a mtehod called set_report_frequency
+def set_report_frequency
 #prompt report_frequency setting
 # This will be the number of times the user has to report durring the goal period.
 puts "How often would you like to record your progress?"
 report_frequency = gets.chomp 
+end 
 
-puts "how many times a week would you like to #{action}"
-times_of_week = gets.chomp
-
-puts "how long would you like to carry on #{action} each time?"
