6002c9e6cbf99dc7a6b92f5d8c3e914f7e5947b2
Bryce harlum <bryce.harlum@gmail.com>
2019-03-06 09:45:31 +1100

    Reformatted some comments into a style, ran tests on input that required a number or a specific string using the 'valid_input' class

--- classes/goal_prompts.rb
+++ classes/goal_prompts.rb
@@ -1,13 +1,13 @@
 require_relative 'valid_input'
-## Tests the given string to see if it equals 'yes'
-## Converts given string to downcase to accept any combination of case
-### Returns true if equal to "yes" otherwise returns false
+
+
+# Tests the given string to see if it equals 'yes'
+# Converts given string to downcase to accept any combination of case
+# Returns true if equal to "yes" otherwise returns false
+#
 class GoalPrompts
     
     vi_ref = ValidInput.new
-    def is_num(var)
-        var.numeric?
-    end
 
     def initialize
         @goal = ""
@@ -19,53 +19,80 @@ class GoalPrompts
 
     # create a method called new_goal
     # prompts users to enter their goal
-    #create a control flow of yes and no after taking in users' answer input 
-    #to see if they would like to continue the program
+    # create a control flow of yes and no after taking in users' answer input 
+    # to see if they would like to continue the program
+    #
     def new_goal
-        puts "Hi, would you like to create a new goal? [yes/no]" 
-        input = gets.chomp 
-        
-        if !is_yes?(input)
+        input = nil
+        until vi_ref.is_valid_text?(input)
+            puts "Hi, would you like to create a new goal? [yes/no]" 
+            input = gets.chomp 
+        end
+        unless vi_ref.is_yes?(input)
             puts "As you do not want to set a goal; Exiting program"
             exit
         end
     end
 
-    #create a method called choose_goal
-    #prompt goal setting
+    # create a method called choose_goal
+    # prompt goal setting
+    #
     def choose_goal
         puts "What goal would you like to set today?" 
         @goal = gets.chomp
     end 
     #To do: give options to goals as exercise more, read more, eat healthier
 
-    #create a method called choose_action
-    #prompt action setting
+    # create a method called choose_action
+    # prompt action setting
+    #
     def choose_action
-        puts "what action would you like to do to work towards your goal?"
+        puts "What action would you like to do to work towards your goal?"
         @action = gets.chomp 
     end 
     ##To do:give options to actions as swimming, jogging,  gym
 
-    #create a method called set_times
-    def set_time
-        puts "how many times a week would you like to go #{@action}"
-        @times_of_week = gets.chomp
+    # create a method called set_times
+    # Gather input
+    # tests input: is numeric and greater than -1
+    # stores input
+    #
+    def set_times
+        input = nil
+        until vi_ref.is_num_pos?(input)
+        puts "How many times a week would you like to go #{@action}"
+        input = gets.chomp
+        end
+        @times_of_week = input
     end 
 
-    #create a method called set_duration
+    # Create a method called set_duration
+    # Gather input
+    # tests input: is numeric and greater than -1
+    # stores input
+    #
     def set_duration
-        puts "how long would you like to carry on your #{@action} each time(minutes)?"
-        @duration_of_action = gets.chomp
+        input = nil
+        until vi_ref.is_num_pos?(input)
+        puts "How long would you like to carry on your #{@action} each time(minutes)?"
+        input = gets.chomp
+        end
+        @duration_of_action = input
     end
-    ##To do: add condition to enter in minutes
 
     # create a method called set_report_frequency
-    # prompt report_frequency setting
     # This will be the number of times the user has to report durring the goal period.
+    # Gather input
+    # tests input: is numeric and greater than -1
+    # stores input
+    #
     def set_report_frequency
-        puts "How often would you like to record your progress per week?"
-        @report_frequency = gets.chomp 
+        input = nil
+        until vi_ref.is_num_pos?(input)
+            puts "How often would you like to record your progress per week?"
+            input = gets.chomp 
+        end
+        @report_frequency = input
     end 
 
 end 
--- classes/valid_input.rb
+++ classes/valid_input.rb
@@ -1,10 +1,11 @@
-#class that checks to see if input is expected.
+#class that checks to see if user input is expected.
 class valid_input
 
     # takes 2 integers and 1 input
     # checks to see if input is a numeric
     # then checks to see if inputs value lies within the range of bot and top
     # returns boolean
+    #
     def is_num_range?(input, bot, top)
         input.numeric? && input >= bot && input <= top ? true : false
     end
@@ -12,6 +13,7 @@ class valid_input
     # takes 2 arguments string and array
     # If input is equal to an element in the array of expected input, return true.
     # returns boolean
+    #
     def is_valid_text?(input, arr)
         result = false
         arr.each do |ele|
@@ -24,6 +26,7 @@ class valid_input
     # checks to see if it is a numeric
     # checks to see if input is greater than -1
     # returns boolean
+    #
     def is_num_pos?(input)
         input.numeric? && input > -1 ? true : false
     end
@@ -32,6 +35,7 @@ class valid_input
     # chacks to see if input is equal to 'yes'
     # returns boolean
     # returns boolen
+    #
     def is_yes?(input)
         input.downcase == "yes" ? true : false
     end 
