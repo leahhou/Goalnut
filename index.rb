78a1a5e5c5f7afbd3d2b09a72eb57531c0d77358
Leah Hou <hsx_124@hotmail.com>
2019-03-06 13:53:54 +1100

    add methods in index to call reward

--- index.rb
+++ index.rb
@@ -19,3 +19,13 @@ g.calc_result_duration_perc
 g.calc_result_times_perc
 g.calc_result_duration_average
 
+r = Reward.new
+g.perc_duration_achieved
+g.perc_times_achieved
+if g.perc_duration_achieved >= 100 and g.perc_times_achieved >= 100
+    r.give_a_goalnut
+end 
+if g.perc_duration_achieved < 100 or g.perc_times_achieved < 100
+    r.give_some_sprinkles
+end
+
